ca5486af631a8ae47cbd2bf16d6835bb1337b1b6
simonmartyr <simonmartyr@hotmail.co.uk>
2019-02-05 22:02:39 +0100

    removed uneeded

--- Controllers/RabbitController.cs
+++ Controllers/RabbitController.cs
@@ -15,34 +15,27 @@ using RabbitMQ.Client;
 
 namespace MicroService.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class RabbitController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RabbitController : ControllerBase
+  {
+    private readonly RabbitManager rabbitManager;
+    public RabbitController(RabbitManager rabbit)
     {
-        private readonly RabbitManager rabbitManager;
-        public RabbitController(RabbitManager rabbit)
-        {
-          rabbitManager = rabbit;
-        }
-        // GET api/values
-        [HttpGet]
-        public ActionResult<string> Get()
-        {
-            return "nothing he";
-        }
-
-        // GET api/values/5
-        // [HttpGet("{id}")]
-        // public  ActionResult<Debtor> Get(string id)
-        // {
-        //     return database.RetrieveItem(id);
-        // }
-
-        // POST api/values
-        [HttpPost]
-        public void Post(RabbitMessage value)
-        {
-          rabbitManager.Publish(value.Key, value.Message);
-        }
+      rabbitManager = rabbit;
     }
+    // GET api/values
+    [HttpGet]
+    public ActionResult<string> Get()
+    {
+      return "nothing he";
+    }
+
+    // POST api/values
+    [HttpPost]
+    public void Post(RabbitMessage value)
+    {
+      rabbitManager.Publish(value.Key, value.Message);
+    }
+  }
 }
