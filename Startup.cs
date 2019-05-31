bc8fa7ab421a3c013263c1b690094d6cdefa1f04
Thomas <thomas.poiesz@finaps.nl>
2019-05-31 14:27:32 +0200

    Add logging for validation issues

--- Startup.cs
+++ Startup.cs
@@ -32,6 +32,23 @@ namespace MicroService
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      // This code is used to configure what happens when the modelstate is invalid (validation errors)
+      // Right now it just logs and then returns the errors to the client
+      services.PostConfigure<ApiBehaviorOptions>(options =>
+      {
+        var builtInFactory = options.InvalidModelStateResponseFactory;
+
+        options.InvalidModelStateResponseFactory = context =>
+        {
+          var loggerFactory = context.HttpContext.RequestServices
+            .GetRequiredService<ILoggerFactory>();
+          var logger = loggerFactory.CreateLogger(context.ActionDescriptor.DisplayName);
+          var errorString = string.Join("\n", context.ModelState.SelectMany(x => x.Value.Errors).Select(x => x.ErrorMessage));
+          logger.LogError("Validation error on incoming request:\n{0}", errorString);
+
+          return builtInFactory(context);
+        };
+      });
       services.Configure<RabbitOptions>(Configuration.GetSection("Rabbit"));
       services.AddSingleton<IConfiguration>(Configuration);
       services.AddRabbitMQ(Configuration);
